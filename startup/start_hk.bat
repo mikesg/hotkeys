bcb9638d013bd34bacdab4e26022ee0572ad8edc
Mike Grigorov <mikesg@abv.bg>
2014-09-22 09:09:16 +0300

    Remove volume script ftom startup

--- startup/start_hk.bat
+++ startup/start_hk.bat
@@ -1,2 +1,2 @@
 start "C:\Program Files\AutoHotkey\AutoHotkeyU32.exe" "engineering frequent tasks.ahk"
-start "C:\Program Files\AutoHotkey\AutoHotkeyU32.exe" "volume with mouse.ahk"
+rem start "C:\Program Files\AutoHotkey\AutoHotkeyU32.exe" "volume with mouse.ahk"
